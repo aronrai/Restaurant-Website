6c83d35c3228eee935ee852be70ae14577bf02fb
Aron Rai <aron.dumi.rai@gmail.com>
2024-12-03 12:27:21 +0530

    Updated jS

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const menuIcon = document.querySelector(".menu-icon");
 const dropdownMenu = document.querySelector("#dropdownMenu");
-menuIcon.addEventListener("click", () => {
+
+// Toggle dropdown menu when clicking the menu icon
+menuIcon.addEventListener("click", (event) => {
+  event.stopPropagation(); // Prevent event from bubbling up to the document
   dropdownMenu.style.display =
     dropdownMenu.style.display === "flex" ? "none" : "flex";
+});
+
+// Close the dropdown menu when clicking anywhere else on the page
+document.addEventListener("click", () => {
+  if (dropdownMenu.style.display === "flex") {
+    dropdownMenu.style.display = "none";
+  }
 });
 
 document.querySelector("#year").textContent = new Date().getFullYear();
